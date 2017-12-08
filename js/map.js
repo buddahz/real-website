3bf20a19b85ac409eb95b42b62044ec9b6b31aeb
José Bonifacio <jbonifacio85@gmail.com>
2017-12-08 20:04:31 +0000

    Updated 'js/map.js' via CloudCannon

--- js/map.js
+++ js/map.js
@@ -139,3 +139,10 @@ function init_map() {
 	});
 }
 google.maps.event.addDomListener(window, 'load', init_map);
+
+$(function() {
+	$('a[href*=#]').on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+	});
+});
